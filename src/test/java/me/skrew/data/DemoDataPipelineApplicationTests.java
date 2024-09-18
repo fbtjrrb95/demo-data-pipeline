462fef0cb679aeb74e394931b90c57a3ffefbb34
seokkyu <tjrrb95@gmail.com>
2024-09-18 23:40:54 +0900

    build(): first commit

---
+++ src/test/java/me/skrew/data/DemoDataPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package me.skrew.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDataPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
