bc1e9cc03f50d16df6a7e0314236826c32c95951
seokkyu <tjrrb95@gmail.com>
2024-10-22 00:18:05 +0900

    refactor(storeESConsumer): inject elasticsearch config dynamically from application-local.yml

--- src/main/java/me/skrew/data/consumer/es/ElasticSearchConfiguration.java
+++ src/main/java/me/skrew/data/consumer/es/ElasticSearchConfiguration.java
@@ -5,6 +5,7 @@ import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
@@ -13,9 +14,18 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories
 public class ElasticSearchConfiguration {
 
+    @Value("${elasticsearch.hostname}")
+    private String hostname;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Value("${elasticsearch.scheme}")
+    private String scheme;
+
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200, "http")).build();
+        RestClient restClient = RestClient.builder(new HttpHost(hostname, port, scheme)).build();
         RestClientTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         return new ElasticsearchClient(transport);
     }
